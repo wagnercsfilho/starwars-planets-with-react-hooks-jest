7277d09ab2ca983aca9db743ac05076bfa416937
Wagner CS Filho <wagnercsfilho@hotmail.com>
2020-10-15 16:14:23 -0300

    fix delete

--- src/components/Planet.jsx
+++ src/components/Planet.jsx
@@ -4,7 +4,7 @@ export const Planet = ({ planet, remove }) => {
   return (
     <div className="planet-item">
       <div>{planet.name}</div>{" "}
-      <button type="button" onClick={remove(planet)}>
+      <button type="button" onClick={remove}>
         remove
       </button>
     </div>
